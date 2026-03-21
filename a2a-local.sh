adb4680848ca4de1a0057058195c3be40f5925f8
xbill <xbill9@gmail.com>
2026-03-21 13:09:52 -0400

    clean up

---
+++ a2a-local.sh
@@ -0,0 +1,7 @@
+source $HOME/a2a-benchmark/set_env.sh
+
+cd src/agents/a2a_local_agent
+
+echo `pwd`
+echo local a2a agent
+python agent.py
---
+++ a2a-master.sh
@@ -0,0 +1,7 @@
+source $HOME/a2a-benchmark/set_env.sh
+
+cd src/agents/a2a_master_agent
+
+echo `pwd`
+echo master a2a agent
+python agent.py
